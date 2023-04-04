31f4489410b96624a24de591b20d8d9627943c09
RekyanMaulana <rekyan.maulana@gmail.com>
2023-04-04 20:55:51 +0700

    upload folder admin

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<?php
+    //memanggil dan memproses file bagian atas
+    include_once 'top.php';
+    //memanggil dan memproses file bagian menu
+    include_once 'menu.php';
+?>
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1>Selamat Datang di Web Admin</h1>
+            <?php
+            error_reporting(0);
+            $url = $_GET['url'];
+            if($url == 'dashboard'){
+                include_once 'dashboard.php';
+            } else if (!empty($url)){
+                include_once ''.$url.'.php';
+            } else {'dashboard.php';
+            }
+            ?>
+        </div>
+    </main>
+</div>
+
+<?php
+    //ini memanggil file bagian bawah
+    include_once 'bottom.php';
+?>
